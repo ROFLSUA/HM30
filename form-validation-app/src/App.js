d0f7054273985e849f3dd1c9d3fca11b727e146c
Dania <135432914+ROFLSUA@users.noreply.github.com>
2023-10-19 15:06:54 +0300

    Add files via upload

---
+++ form-validation-app/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import FormComponent from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Форма з валідацією</h1>
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
